d27ce56dc903d413c551f4fdaf1322ecd2f786af
zwsatan <zwsatan420@163.com>
2014-12-26 21:30:55 +0800

    调整工具脚本

--- tools/makeflist/main.lua
+++ tools/makeflist/main.lua
@@ -3,8 +3,7 @@ require("makeFlist")
 
 if device.platform == "windows" then
 	-- 休眠3秒
-	os.execute("ping -n 3 127.0.0.1>nul")
+	os.execute("ping -n 1 127.0.0.1>nul")
 	-- 关闭此进程
 	os.execute("tskill player3")
-else device.platform == "mac" then
 end

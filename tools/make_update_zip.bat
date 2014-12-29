39b481aeee00d0f096c850a679a08e31c7fb2518
zwsatan <zwsatan420@163.com>
2014-12-29 11:04:28 +0800

    调整工具脚本，提交资源底图
    1、添加技能框资源底图
    2、updater打包时，针对zip包加密
    3、更新服务端updater.zip

--- tools/make_update_zip.bat
+++ tools/make_update_zip.bat
@@ -1,1 +1,1 @@
-%QUICK_V3_ROOT%quick/bin/compile_scripts.bat -i %~dp0../src/module/updater/ -o %~dp0../res/updater.zip
+%QUICK_V3_ROOT%quick/bin/compile_scripts.bat -i %~dp0../src/module/updater/ -o %~dp0../res/updater.zip -e xxtea_zip -ek zwsatan
